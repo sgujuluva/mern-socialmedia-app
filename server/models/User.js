a246ee9a01da6e4f1c89bc45c5b146a73f641af4
sgujuluva <sanghee04@gmail.com>
2022-12-05 21:02:10 +0100

    user schema generated

---
+++ server/models/User.js
@@ -0,0 +1,45 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema({
+    username:{
+        type:String,
+        required:true,
+        min:3,
+        max:20,
+        unique:true
+    },
+    email:{
+        type:String,
+        required:true,
+        max:50,
+        unique:true
+    },
+    password:{
+        type:String,
+        required:true,
+        min:6
+    },
+    profilePicture:{
+        type:String,
+        default:""
+    },
+    coverPicture:{
+        type:String,
+        default:""
+    },
+    followers:{
+        type:Array,
+        default:[]
+    },
+    followins:{
+        type:Array,
+        default:[]
+    },
+    isAdmin:{
+        type:Boolean,
+default:false
+    }
+
+})
+
+const User = mongoose.model("User",UserSchema)
