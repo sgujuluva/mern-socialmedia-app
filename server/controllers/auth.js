ba20efc1025cbea33307935ef835cc7e47cd657a
sgujuluva <sanghee04@gmail.com>
2022-12-06 14:16:00 +0100

    registering new user works

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -3,33 +3,21 @@ import User from "../models/User.js"
 export const registerUser = async (req,res) => {
     const {username,email,password}=req.body
     console.log(req.body.username)
-     try {
+      try {
          const checkUserNameExist = await User.findOne({email:req.body.email})
 
         if(checkUserNameExist){
            return res.send("The user already registered")
-        }else{ 
-            const createUser = await new User.create({
+        }else{  
+            const createUser = await User.create({
                 username:username,
                 email:email,
                 password:password
             })
-             // return res.status(200).json(createUser)
-            return res.send(createUser)
-       }
+              return res.status(200).json(createUser)
+                  }
     } catch (error) {
         return res.send(error)
-    } 
+    }  
 
 }
-/*  const createUser = await new User({
-             username:req.body.username,
-             email:req.body.email,
-             password:req.body.password
-         })
-         try{
-             const user = await newUser.save()
-             res.status(200).json(user)
-         }catch(error){
-             console.log(error)
-         } */
